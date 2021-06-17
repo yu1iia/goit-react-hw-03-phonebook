a642bfe6ffeff0f8aa0fc73f7f92b19e586f8ed1
Yuliia Burko <yuliia.burko@gmail.com>
2021-06-18 00:18:05 +0200

    added Local Storage

--- src/App.js
+++ src/App.js
@@ -29,10 +29,10 @@ class App extends Component {
   //Local Storage
 
   componentDidMount() {
-    const contacts = localStorage.getItem('components');
-    const parsedContacts = JSON.parse(contacts);
+    const addedContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(addedContacts);
 
-    if (parsedContacts) {
+    if (addedContacts !== null) {
       this.setState({
         contacts: parsedContacts,
       });
@@ -45,7 +45,9 @@ class App extends Component {
 
     if (nextContacts !== prevContacts) {
       console.log('Обновилось поле contacts, записываю todos в хранилище');
-      localStorage.setItem('contacts', JSON.stringify(nextContacts));
+      if (prevState.contacts !== this.state.contacts) {
+        localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+      }
     }
   }
 
@@ -58,7 +60,9 @@ class App extends Component {
       alert(`${newContact.name} is already in contacts!`);
       return;
     }
-    this.setState({ contacts: [newContact, ...contacts] });
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts],
+    }));
   };
 
   handleFilterContacts = ({ currentTarget }) => {
